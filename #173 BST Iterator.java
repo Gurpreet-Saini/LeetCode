75766a284843cff7392be242076baaf4df7dce3e
Gurpreet-Saini <66106424+Gurpreet-Saini@users.noreply.github.com>
2022-04-20 23:52:17 +0530

    Update #173 BST Iterator.java

--- #173 BST Iterator.java
+++ #173 BST Iterator.java
@@ -1,23 +1,48 @@
-public class BSTIterator {
-    private Stack<TreeNode> stack = new Stack<TreeNode>();
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */ 
+ 
+ ////
+ ///   PAGE 4-5 
+ ///
+class BSTIterator {
+         private Stack <TreeNode> stack= new Stack<>();
+
     
     public BSTIterator(TreeNode root) {
         pushAll(root);
     }
-
-    /** @return whether we have a next smallest number */
+    
+    public int next() {
+        TreeNode node = stack.pop();
+        pushAll(node.right);
+        return node.val;
+    }
+    
     public boolean hasNext() {
         return !stack.isEmpty();
     }
-
-    /** @return the next smallest number */
-    public int next() {
-        TreeNode tmpNode = stack.pop();
-        pushAll(tmpNode.right);
-        return tmpNode.val;
-    }
-    
-    private void pushAll(TreeNode node) {
-        for (; node != null; stack.push(node), node = node.left);
+    private void pushAll(TreeNode root){
+        for(; root!=null ; stack.push(root),root=root.left );
+            
     }
 }
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
