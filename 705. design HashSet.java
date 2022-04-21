1362b2b4c0d561c8a272c3ae15ec78032689fa55
Gurpreet-Saini <66106424+Gurpreet-Saini@users.noreply.github.com>
2022-04-21 15:10:13 +0530

    Update 705. design HashSet.java

--- 705. design HashSet.java
+++ 705. design HashSet.java
@@ -1,5 +1,5 @@
- Using ArrayList
-```
+ //Using ArrayList
+
 class MyHashSet {
    ArrayList<Integer> list ;
     public MyHashSet() {
@@ -25,4 +25,46 @@ class MyHashSet {
             return false;
     }
 }
-```
+   
+///USING LINKEDLIST 
+
+class MyHashSet {
+        private final int max=100000;
+    private final int arr_size=100;
+    private List<List<Integer>> parentList;
+    public MyHashSet() {
+        parentList = new ArrayList<>(arr_size);
+        for( int i=0 ;i<arr_size;i++){
+            parentList.add(null);
+        }
+    }
+    
+    public void add(int key) {
+        int index= key%arr_size;
+        List<Integer> childList= parentList.get(index);
+        if(childList== null){
+            List<Integer> list= new LinkedList<>();
+            list.add(key);
+            parentList.set(index, list);
+        }else{
+                if(!childList.contains(key)){
+                    childList.add(key);
+                }
+        }
+        
+    }
+    
+    public void remove(int key) {
+       int index= key%arr_size;
+        List<Integer> childList= parentList.get(index);
+        if(childList!=null){
+            childList.remove(Integer.valueOf(key));
+        }
+    }
+    
+    public boolean contains(int key) {
+       int index = key %arr_size;
+        List<Integer> childList= parentList.get(index);
+        return childList!=null && childList.contains(key);
+    }
+}
